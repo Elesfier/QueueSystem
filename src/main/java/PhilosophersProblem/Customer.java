231063110219d5f5de0237396d48091474c4638c
Artur Szajdecki <artur.szajdecki@gmail.com>
2019-01-11 02:37:14 +0100

    working philosopher problem as distributed system

--- src/main/java/PhilosophersProblem/Customer.java
+++ src/main/java/PhilosophersProblem/Customer.java
@@ -6,83 +6,127 @@ import java.util.Random;
 
 public class Customer extends LocalService {
 
-    private long sendingIndex;
-    private long ownIndex;
-    private long indexOfCustomer;
+    enum StatusFork {
+        NONE,
+        DIRTY,
+        CLEAN
+    }
 
-    Customer( long index ) {
-        super("Customer","CUSTOMER" + Long.toString(index));
+    //Own Fork
+    private StatusFork ownFork;
+
+    //Neighbour depend Fork
+    private StatusFork dependFork;
+
+    //Own Index
+    private int ownIndex;
+
+    //Neighbour depend Index
+    private int dependIndex;
+
+    private boolean isPreparingToEat;
+    private int wantedIndex;
+
+
+    Customer( int index, int dependIndex ) {
+        super("Customer","CUSTOMER" + Integer.toString(index));
         this.ownIndex = index;
-        this.indexOfCustomer = 0;
-        this.sendingIndex = 0;
+        this.wantedIndex = -1;
+        this.dependIndex = dependIndex;
+        this.ownFork = StatusFork.DIRTY;
+        this.dependFork = StatusFork.NONE;
+        this.isPreparingToEat = false;
     }
 
     @Override
     public void onReceivedMessage(String request, String[] messages) {
         try {
 
-            long index = Long.parseLong(request.substring(request.indexOf( "CUSTOMER" ), request.length()));
+            int index = Integer.parseInt(request.substring("CUSTOMER".length(), request.length()));
 
-            if ( indexOfCustomer == ownIndex ) {
-                System.out.println(getName() + ": is busy sending.");
-            } else if ( indexOfCustomer == 0 ) {
-                indexOfCustomer = index;
-                System.out.println(getName() + ": is now provider && received.");
-                receivedData( messages );
-                if (messages[ messages.length-1 ].equals("END")) {
-                    indexOfCustomer = 0;
-                }
-            } else if ( (indexOfCustomer == index) && (sendingIndex == 0) ) {
-                System.out.println(getName() + ": received.");
-                receivedData( messages );
-                if (messages[ messages.length-1 ].equals("END")) {
-                    indexOfCustomer = 0;
-                }
-
-            } else {
-                System.out.println(getName() + ": is busy.");
+            switch (messages[0]) {
+                case "GIVE_BACK_FORK":
+                    //System.out.println( "GIVE_BACK_FORK" );
+                    ownFork = StatusFork.CLEAN;
+                    if ( ownFork.equals(StatusFork.CLEAN) && !dependFork.equals(StatusFork.NONE) ) {
+                        eatTask();
+                    }
+                    break;
+                case "GIVE_FORK":
+                    //System.out.println( "GIVE_FORK" );
+                    if ( index == dependIndex ) {
+                        dependFork = StatusFork.CLEAN;
+                        if ( dependFork.equals(StatusFork.CLEAN) && !ownFork.equals(StatusFork.NONE) ) {
+                            eatTask();
+                        }
+                    } else {
+                        System.out.println( getName() + ": strange index.");
+                    }
+                    break;
+                case "GIVE_ME_FORK":
+                    //System.out.println( "GIVE_ME_FORK" );
+                    if (ownFork.equals(StatusFork.DIRTY) ) {
+                        this.ownFork = StatusFork.NONE;
+                        String[] message = { "GIVE_FORK" };
+                        sendMessage("CUSTOMER" + Integer.toString( index ), message );
+                    } else {
+                        wantedIndex = index;
+                    }
+                    break;
+                default:
+                    System.out.println( getName() + ": strange command.");
             }
 
         } catch ( Exception e ) {
-            System.out.println( getName() + ": strange message.");
+            System.out.println("Exception: " + e.getMessage());
+            System.out.println( getName() + ": strange message >> " + request + " >> " + String.join(", ", messages));
         }
     }
 
-    public void receivedData( String[] messages ) {
-        System.out.println(getName() + String.join("", messages));
-    }
+    private void eatTask() {
 
+        System.out.println( getName() + ": Eating");
+        //TODO
 
-    public boolean canSendMessages() {
-        if ( indexOfCustomer == 0 ) {
-            indexOfCustomer = ownIndex;
+        dependFork = StatusFork.DIRTY;
+
+        if ( wantedIndex != -1 ) {
+            ownFork = StatusFork.NONE;
+            String[] secondMessage = { "GIVE_FORK" };
+            sendMessage("CUSTOMER" + Integer.toString( wantedIndex ), secondMessage );
+            wantedIndex = -1;
+        } else {
+            ownFork = StatusFork.DIRTY;
         }
+
+        //isPreparingToEat = false;
     }
 
-    public void stopSending() {
-        indexOfCustomer = 0;
+    public void periodTask() {
+        try {
+            Thread.sleep(2000);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println( "ownFork: " + ownFork + " dependFork: " + dependFork );
+
+        //Want to eat
+        String[] message = { "GIVE_ME_FORK" };
+        sendMessage("CUSTOMER" + Integer.toString( dependIndex ), message );
+        if ( ownFork.equals( StatusFork.NONE ) ) {
+            String[] secondMessage = { "GIVE_BACK_FORK" };
+            sendMessage("CUSTOMER" + Integer.toString( wantedIndex ), secondMessage );
+        }
+        //isPreparingToEat = true;
     }
 
     public static void main( String[] args ) {
-        Customer customer = new Customer( Long.parseLong(args[1]) );
+        Customer customer = new Customer(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
         customer.start();
 
-        Random rand = new Random();
-
         while( true ) {
-
-            try {
-                Thread.sleep(3000);
-            } catch(Exception e) {
-                System.out.println(e.getMessage());
-            }
-
-            int index = rand.nextInt(5) + 1;
-            if ( customer.canSendMessages() ) {
-                String[] message = { "user", "password", "investement", "BLABLA", "END" };
-                customer.sendMessage("CUSTOMER" + Integer.toString( index ), message );
-                customer.stopSending();
-            }
+            customer.periodTask();
         }
     }
 }
